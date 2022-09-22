c57cd0a22321a20cb4f5e200d73a2a1503594e7e
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-22 20:42:29 +0300

    chore: created pin_code_page.dart

---
+++ lib/app/login/views/pages/pin_code_page.dart
@@ -0,0 +1,63 @@
+import 'package:attendance_with_location/app/login/controllers/login_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class PinCodePage extends StatelessWidget {
+  final LoginController controller;
+
+  const PinCodePage({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 40,
+      ),
+      child: PinCodeTextField(
+        errorTextSpace: 30,
+        validator: controller.smsValidator,
+        appContext: context,
+        autoFocus: true,
+        autoDisposeControllers: true,
+        length: 4,
+        obscureText: false,
+        animationType: AnimationType.scale,
+        cursorHeight: 25,
+        keyboardType: TextInputType.number,
+        pinTheme: PinTheme(
+          fieldHeight: 55,
+          fieldWidth: 40,
+          shape: PinCodeFieldShape.box,
+          borderRadius: BorderRadius.circular(5),
+          activeColor: Colors.white,
+          selectedFillColor: Colors.white,
+          inactiveFillColor: Colors.white,
+          inactiveColor: Colors.white,
+          selectedColor: Colors.white,
+          activeFillColor: Colors.white,
+        ),
+        cursorColor: Colors.black,
+        boxShadows: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 8,
+            blurRadius: 6,
+            offset: const Offset(0, 4),
+          ),
+        ],
+        enableActiveFill: true,
+        controller: controller.smsCodeController,
+        onCompleted: (value) {
+          controller.navigateToNextPage();
+        },
+        onChanged: (value) {},
+        beforeTextPaste: (text) {
+          return true;
+        },
+      ),
+    );
+  }
+}
