71b189dfd1969ba4eb15be7a576d45c4e259e233
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-22 20:42:18 +0300

    chore: created phone_nymber_page.dart

---
+++ lib/app/login/views/pages/phone_number_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:attendance_with_location/app/login/controllers/login_controller.dart';
+
+class PhoneNumberPage extends StatelessWidget {
+  final LoginController controller;
+
+  const PhoneNumberPage({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IntlPhoneField(
+      controller: controller.phoneController,
+      decoration: const InputDecoration(
+        labelText: 'Phone Number',
+        border: OutlineInputBorder(
+          borderSide: BorderSide(),
+        ),
+      ),
+      initialCountryCode: 'SA',
+    );
+  }
+}
