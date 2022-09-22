c21a88edfc9e06b108aff7ca44d20c47fa23598e
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-22 20:42:39 +0300

    chore: created login_view.dart

---
+++ lib/app/login/views/login_view.dart
@@ -0,0 +1,99 @@
+import 'package:attendance_with_location/app/login/controllers/login_controller.dart';
+import 'package:attendance_with_location/app/login/services/login_services.dart';
+import 'package:attendance_with_location/app/login/views/pages/phone_number_page.dart';
+import 'package:attendance_with_location/app/login/views/pages/pin_code_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late LoginController controller;
+
+  @override
+  initState() {
+    controller = LoginController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              top: 150,
+              child: Row(
+                children: const [
+                  Text(
+                    'Quiz U',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 30,
+                    ),
+                  ),
+                  Icon(
+                    Icons.timelapse_rounded,
+                    size: 35,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 350,
+              right: 35,
+              left: 35,
+              height: 150,
+              child: PageView(
+                physics: const NeverScrollableScrollPhysics(),
+                controller: controller.pageController,
+                children: [
+                  PhoneNumberPage(
+                    controller: controller,
+                  ),
+                  PinCodePage(
+                    controller: controller,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 200,
+              right: 80,
+              left: 80,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    Colors.blueGrey,
+                  ),
+                ),
+                onPressed: () async {
+                  controller.navigateToNextPage();
+                  print(await LoginServices.loginWithPhoneNumber(''));
+                },
+                child: const SizedBox(
+                  height: 45,
+                  child: Center(
+                    child: Text(
+                      'next',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
