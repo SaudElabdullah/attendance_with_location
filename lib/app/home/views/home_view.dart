f742909dd1a26addd940128854eb35892f7a152d
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-24 00:24:06 +0300

    chore: created home_view.dart

---
+++ lib/app/home/views/home_view.dart
@@ -0,0 +1,37 @@
+import 'package:attendance_with_location/app/home/controllers/home_controller.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late HomeController controller;
+
+  @override
+  void initState() {
+    controller = HomeController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Positioned(
+              child: Text(
+                controller.inPlace ? 'You are in place' : 'You are not in place',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
