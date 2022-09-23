d723ad46059b2b08abaaf56c9b0fea9d7d0b03d8
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-24 00:25:16 +0300

    chore: added login

--- lib/app/login/controllers/login_controller.dart
+++ lib/app/login/controllers/login_controller.dart
@@ -1,13 +1,28 @@
+import 'package:attendance_with_location/app/home/views/home_view.dart';
+import 'package:attendance_with_location/app/login/services/login_services.dart';
+import 'package:attendance_with_location/core/utils/location_util.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginController {
-  final PageController pageController = PageController(
-    initialPage: 0,
-    keepPage: true,
-  );
-  final phoneController = TextEditingController();
-  final smsCodeController = TextEditingController();
-  final nameController = TextEditingController();
+  late PageController pageController;
+  late BuildContext context;
+  late TextEditingController phoneController;
+  late TextEditingController smsCodeController;
+  late LoginServices loginServices;
+  late String countryKey;
+
+  LoginController(this.context) {
+    pageController = PageController(
+      initialPage: 0,
+      keepPage: true,
+    );
+    phoneController = TextEditingController();
+    smsCodeController = TextEditingController();
+    loginServices = LoginServices();
+    countryKey = '+966';
+    LocationUtil.determinePosition();
+  }
 
   String? smsValidator(String? sms) {
     if (((sms?.length ?? 0) < 6) || sms != '0000') return "Incorrect SMS Code";
@@ -21,5 +36,37 @@ class LoginController {
       ),
       curve: Curves.ease,
     );
+  }
+
+  Future<void> loginController() async {
+    String page = pageController.page!.toString();
+    switch (page) {
+      case "0.0":
+        {
+          phoneController.text = countryKey + phoneController.text;
+          loginServices.login(phoneController.text);
+          navigateToNextPage();
+        }
+        break;
+
+      case "1.0":
+        {
+          if (await loginServices.verifyCode(smsCodeController.text) != '') {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HomeView(),
+              ),
+            );
+          }
+        }
+        break;
+
+      default:
+        {
+
+        }
+        break;
+    }
   }
 }
