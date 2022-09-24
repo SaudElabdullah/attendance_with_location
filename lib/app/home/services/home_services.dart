242c7f8c488dc62c9a4dac77ab1dfcd0279c8f8f
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-24 23:08:34 +0300

    fix: fix an issue

--- lib/app/home/services/home_services.dart
+++ lib/app/home/services/home_services.dart
@@ -1,13 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 
 class HomeServices {
   static void recorde(String recordeName) async {
     String uid = FirebaseAuth.instance.currentUser!.uid;
     DateTime date = DateTime.now();
-    String dateNow = '${date.year}-${date.month}-${date.day}';
+    String dateNow = DateFormat.yMMMMd().format(date);
     FirebaseFirestore.instance.collection("employees_attendance").doc(uid).collection('attendance').doc(dateNow).set(
-      {DateTime.now().toString(): recordeName},
+      {DateFormat.yMMMd().add_jm().format(DateTime.now()).toString(): recordeName},
       SetOptions(merge: true),
     );
   }
