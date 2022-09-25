8b69cb36fe1ce074f77bb5494dfae51cfca5bea7
Saud Elabdullah <saudelabdullah@gmail.com>
2022-09-25 18:22:35 +0300

    chore: Modify UI

--- lib/app/login/views/login_view.dart
+++ lib/app/login/views/login_view.dart
@@ -26,6 +26,7 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Stack(
           alignment: Alignment.topCenter,
